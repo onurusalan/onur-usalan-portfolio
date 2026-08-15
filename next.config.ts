114876c83f7dd66d8ff2da40bcad930b0f253223
onurusalan <onurusalanoffical@gmail.com>
2026-08-16 02:39:38 +0300

    Configure portfolio image quality

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
+    qualities: [75, 85],
     remotePatterns: [
       {
         protocol: "https",
